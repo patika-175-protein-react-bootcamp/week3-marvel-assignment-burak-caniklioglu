bd992c71cf3fe7e4fee93dcdc91c681ddb982149
burak-caniklioglu <burakcaniklioglu@gmail.com>
2022-03-29 01:43:40 +0300

    Single page

---
+++ marvel-api/src/components/Main/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from './Card/Card';
+import './Main.css';
+
+function Main() {
+    return (
+        <div id='image-area'>
+            <div className="container">
+                <Card/> 
+            </div>
+        </div>
+    );
+}
+
+export default Main;
